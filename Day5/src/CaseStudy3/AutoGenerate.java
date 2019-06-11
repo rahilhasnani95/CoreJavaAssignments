ab9376e483cec4f50deb82cea84626670beda0b5
rahilhasnani95 <rahilhasnani95@gmail.com>
2019-06-11 13:57:29 -0500

    commit

--- Day5/src/CaseStudy3/AutoGenerate.java
+++ Day5/src/CaseStudy3/AutoGenerate.java
@@ -1,10 +1,22 @@
 package CaseStudy3;
 
+class BookingNumber
+{
+	private final int YYY = 115;
+	private char X;
+	private int NNN ;
+	private static int ZZZZ;
+}
+
 public class AutoGenerate {
 
 	public static void main(String[] args) {
 	
-		final int yyy = 115;
+		
+		
+		
+		
+		
 		
 	
 	}
