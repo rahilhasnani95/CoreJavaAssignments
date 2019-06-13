b6cf8f08295e88096fbd29bda79c6685423cb7ae
rahilhasnani95 <rahilhasnani95@gmail.com>
2019-06-12 22:43:38 -0500

    corrected selfreview

--- Day4/src/SelfReview/ClassA.java
+++ Day4/src/SelfReview/ClassA.java
@@ -7,7 +7,7 @@ public class ClassA {
 	public void methodTwo(int i)
 	{	
 	}
-	public static void methodThree(int i)
+	public void methodThree(int i)
 	{	
 	}
 	public static void methodFour(int i)
@@ -17,7 +17,7 @@ public class ClassA {
 
 class classB extends ClassA
 {
-	public static void methodOne(int i)
+	public void methodOne(int i)
 	{
 	}
 	public void methodTwo(int i)
